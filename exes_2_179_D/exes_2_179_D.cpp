00900c149c3907193c6b9e8f57ddb629c17da5f3
dinaamlll <dinamaliaa17@gmail.com>
2023-05-12 11:46:44 +0700

    done

---
+++ exes_2_179_D/exes_2_179_D.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int binarysearch;
+while (d <= a) {
+	int m = d + (a - 1) / 2;
+	if (dina[m] == found)
+		return m;
+	else if (found < dina[m])
+		a = m - 1;
+	else if (found > dina[m])
+		d = m + 1
+}
+
+int main()
+{
+	int dina[39]
+	int n;
+	int i;
+}
+
+void input()
+{
+	while (true)
+	{
+		cout << "Enter the number of elements in the array: ";
+		cin >> n;
+		if ((n > 0) && <= 39))
+		break;
+
+		//Accept array elements
+		cout << "\n------------------\n";
+		cout << "Enter array elements \n";
+		cout << "---------------------\n";
+		for (i = 0; i < n; i++)
+		{
+			cout << "<" << (i + 1) << "<";
+			cin >> dina[i];
+		}
+	return 0;
+}
+
+
+
+
